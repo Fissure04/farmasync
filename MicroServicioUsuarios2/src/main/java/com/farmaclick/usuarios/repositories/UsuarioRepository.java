ff7989fbe932b4a1d718bee10f197741e8205df1
SamuelBaez04 <samuel.valencia.7088@eam.edu.co>
2025-11-17 22:58:53 -0500

    feat: microservicio usuarios add

---
+++ MicroServicioUsuarios2/src/main/java/com/farmaclick/usuarios/repositories/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.farmaclick.usuarios.repositories;
+
+import com.farmaclick.usuarios.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+}
